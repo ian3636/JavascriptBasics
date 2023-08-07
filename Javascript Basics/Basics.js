5dc53ea05a02c70a549d59031a30297b6eac8c9d
Ian Ngaruiya <133209081+ian3636@users.noreply.github.com>
2023-08-07 22:02:29 +0300

    Add files via upload

---
+++ Javascript Basics/Basics.js
@@ -0,0 +1,71 @@
+//Q 1
+var x = 5;
+var y = 10;
+var results = x + y;
+console.log(results) 
+ // = 10;
+
+
+ // Q 2
+ //parseInt parses a string and return the first integer
+
+const number = '20, 30, 40';
+const Add =parseInt(number)
+console.log(Add)
+
+const string = '3000, 2000, 3883';
+const Float = parseFloat(string)
+console.log(Float)
+
+ //Q 3
+ var a = 7;
+ var b = 3;
+ var z = a - b;
+console.log(z)
+
+//Q 4
+
+var var1 = 'Welcome to JavaScript World';
+
+console.log(typeof var1);
+
+
+
+
+//Q 5
+var num1 = 8;
+var num2 = "5";
+var sum = num1 + num2;
+console.log(sum) 
+
+
+//Write a JavaScript function to convert a string into all lowercase letter
+let text = "HELLO WORLD"
+let result = text.toLocaleLowerCase();
+console.log(result)
+
+//How can you concatenate two strings in JavaScript?
+let text1 = "Be"
+let text2 = "Motivated"
+let resul = text1.concat(" ", text2)
+console.log(resul)
+
+//What is the difference between `==` and `===` in JavaScript?
+//  ==used to comparing two variables but ignores the datatype of variable
+
+
+
+//  ===used for comparing two variables but checks datatype and compares two values
+
+
+
+//Write a code snippet to extract the last 3 characters from a given string.
+var tex = "400 383 9398 939 3983 9393"
+var substring = tex.substring(text.length-3)
+console.log(substring)
+
+//How can you determine the length of a string in JavaScript?
+let str = "What a wonderful day";
+let length = str.length;
+console.log(length)
+
